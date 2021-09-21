89f3b9c23327ce6ffd7b1785e3dc1730c1729350
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-21 19:46:10 +0900

    Update submit.php

--- online/submit.php
+++ online/submit.php
@@ -95,6 +95,45 @@ if( !empty($_POST['btn_confirm']) ) {
 <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,300;0,600;0,700;1,600&display=swap" rel="stylesheet">
 <title>大切にすることを大切にする場所</title>
 <style type="text/css">
+#submit h1,
+#submit p,
+#submit button {
+	font-family: 'Source Serif Pro', serif;
+	font-weight:500;
+}
+#submit {
+	position:relative;
+	top:0; left:0;
+	font-size:0.75rem;
+	padding:5% 0;
+}
+#submit section {
+	width:90%;
+	max-width:35rem;
+    margin:auto;
+}
+#submit input[type="text"],
+#submit input[type="name"],
+#submit input[type="url"],
+#submit input[type="email"],
+#submit textarea {
+	padding:1.5%;
+	margin:0.01vw 0;
+	border:0.1rem solid;
+	border-radius:0.5rem;
+	font-size:0.75rem;
+	width:97%;
+}
+#submit button {
+	font-size: 1rem;
+	cursor:pointer;
+	width:100%;
+	margin-bottom:2.5rem;
+	padding:0.25rem 0;
+	background: #fff;
+	color: #000;
+	border-radius:2.5rem;
+}
 </style>
 </head>
 <body>

7f3234d7a1075e14435f5396f105baff753e2568
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-24 13:48:58 +0900

    index

--- index/update.php
+++ index/update.php
@@ -116,16 +116,6 @@ Name</p>
 </div>
 </div>
 <?php endif; ?>
-<div class="en">
-<div class="app">
-<div class="essay" style="text-align:center; font-size:125%;">
-大切にすることを大切にする場所
-</div>
-<div class="link">
-<p><a style="font-size:125%;" href="/value/index/">View the Collection</a></p>
-</div>
-</div>
-</div>
 </div>
 </body>
 </html>

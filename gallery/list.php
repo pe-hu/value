fa9e7c7f3c0af00155c9be7b2e7c1cbf56cb1fa0
∧° ┐ <we.are.pe.hu@gmail.com>
2022-04-04 21:18:58 +0900

    Update list.php

--- gallery/list.php
+++ gallery/list.php
@@ -55,7 +55,7 @@ fclose($fp);
 
 <div id="list">
 <div id="org">
-<p>Index / <a onclick="window.location.reload(true);">大切にすることを大切にするためのギャラリー</a></p>
+<p>Index / 大切にすることを大切にするためのギャラリー</p>
 <ul class="random">
 <?php if (!empty($rows)): ?>
 <?php foreach ($rows as $row): ?>
